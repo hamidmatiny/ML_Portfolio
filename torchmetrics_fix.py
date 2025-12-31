168af9a5016ee2f7a8f5400418905b9a86a10766
hamidmatiny <hamidmatiny@gmail.com>
2025-12-31 13:34:31 -0400

    deleting torchmetrics_fix

--- torchmetrics_fix.py
+++
@@ -1,9 +0,0 @@
-# Fix for torchmetrics Accuracy error
-from torchmetrics import Accuracy
-
-# Setup metric with required 'task' parameter
-torchmetric_accuracy = Accuracy(task='binary').to(device)  # For binary classification
-# For multiclass: Accuracy(task='multiclass', num_classes=num_classes)
-
-# Calculate accuracy
-torchmetric_accuracy(y_preds, y_blob_test)
